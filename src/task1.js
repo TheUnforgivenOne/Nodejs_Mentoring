9c4e493a471acb84682c37bf7ed352451775117c
Vladislav <vladpotapow@yandex.ru>
2021-03-26 17:28:08 +0300

    Task 1 done

---
+++ src/task1.js
@@ -0,0 +1,7 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+rl.on('line', (input) => console.log(input.split('').reverse().join('')));
